a492360d45466ebf4952604ae95653bfeb7db561
louib <code@louib.net>
2021-01-26 19:53:32 -0500

    Adding .inputrc

--- scripts/setup_bash.sh
+++ scripts/setup_bash.sh
@@ -5,8 +5,13 @@ SCRIPT_DIR=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_DIR")
 
 if [ -d "$HOME/git-prompt.sh" ]; then
-    cp "$SCRIPT_DIR/../bash/git-prompt.sh" "$HOME/git-prompt.sh"
+    cp "$SCRIPT_DIR/../assets/bash/git-prompt.sh" "$HOME/git-prompt.sh"
     echo "✔️ Configured Git Prompt."
 else
     echo "✔️ Git Prompt already configured."
+fi
+
+if [[ ! -f "$HOME/.inputrc" ]]; then
+    cp "$SCRIPT_DIR/../assets/bash/.inputrc" "$HOME/.inputrc"
+    echo "✔️ Copied inputrc for readline config."
 fi
