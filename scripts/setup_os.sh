146fb17f9713de8b0fd310f6522f4a72a7af9706
louib <code@louib.net>
2021-02-25 19:56:21 -0500

    Install emojify from GitHub

--- scripts/setup_os.sh
+++ scripts/setup_os.sh
@@ -35,3 +35,19 @@ if ! command -v nvim &> /dev/null; then
 else
     echo "✔️  Main OS packages already installed."
 fi
+
+
+if ! command -v emojify &> /dev/null; then
+    echo "✔️ Installing emojify from GitHub"
+    curl https://raw.githubusercontent.com/mrowa44/emojify/6dc2c1df9a484cf01e7f48e25a1e36e328c32816/emojify -o ./emojify
+    emojify_checksum=$(sha256sum ./emojify | cut -d ' ' -f1)
+    if [[ "$emojify_checksum" != "6ab45a84b7441f802e8a8e6f0979c06043130fad080e5a02c5d8e4d8a32d85ae" ]]; then
+        die "Checksum validation failed for emojify."
+    else
+        echo "✔️ Validated checksum for emojify."
+    fi
+    install emojify /usr/local/bin/
+    echo "✔️ Installed emojify from GitHub"
+else
+    echo "✔️ emojify is already installed."
+fi
