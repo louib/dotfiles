977c8b0701ab37d9cc8070473ec4d76c7c80a0ce
louib <louisv@unito.io>
2021-07-16 13:20:50 -0400

    Remove unused vim plugins.

--- scripts/setup_vim.sh
+++ scripts/setup_vim.sh
@@ -18,19 +18,13 @@ else
 
     # Cloning plugins.
     git clone --recursive https://github.com/morhetz/gruvbox.git ~/.config/nvim/bundle/gruvbox
-    # git clone --recursive https://github.com/sjl/badwolf.git ~/.config/nvim/bundle/badwolf
-    # git clone --recursive https://github.com/ghifarit53/tokyonight-vim.git ~/.config/nvim/bundle/tokyonight
-    # git clone --recursive https://github.com/nanotech/jellybeans.vim ~/.config/nvim/bundle/jellybeans
-    # git clone --recursive http://ethanschoonover.com/solarized ~/.config/nvim/bundle/solarized
     git clone --recursive https://github.com/leafgarland/typescript-vim.git ~/.config/nvim/bundle/typescript-vim
     git clone --recursive https://github.com/pangloss/vim-javascript.git ~/.config/nvim/bundle/vim-javascript
     git clone --recursive https://github.com/python-mode/python-mode.git ~/.config/nvim/bundle/python-mode
     git clone --recursive https://github.com/tpope/vim-surround.git ~/.config/nvim/bundle/vim-surround
-    # git clone --recursive https://github.com/skywind3000/asyncrun.vim ~/.config/nvim/bundle/asyncrun
     git clone --recursive https://github.com/vim-airline/vim-airline ~/.config/nvim/bundle/vim-airline
     git clone --recursive https://github.com/octol/vim-cpp-enhanced-highlight.git ~/.config/nvim/bundle/vim-cpp-enhanced-highlight
     git clone --recursive https://github.com/hashivim/vim-terraform.git ~/.config/nvim/bundle/vim-terraform
-    # git clone --recursive https://github.com/arrufat/vala.vim.git ~/.config/nvim/bundle/vala.vim
     git clone --recursive https://github.com/rust-lang/rust.vim.git ~/.config/nvim/bundle/rust.vim
 
     # Copying config file
