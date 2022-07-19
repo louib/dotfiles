26057ba62c6983dfb6654d5b859e10dda126c87c
Louis Varin <louisvarin@bitgo.com>
2022-07-19 16:49:14 -0400

    Better lua error messages

--- nix/setup_vim.sh
+++ nix/setup_vim.sh
@@ -9,6 +9,19 @@ nix-env -iA nixpkgs.neovim
 nix-env -iA nixpkgs.vimPlugins.nvim-lspconfig
 nix-env -iA nixpkgs.vimPlugins.vim-surround
 nix-env -iA nixpkgs.vimPlugins.nvim-cmp
+nix-env -iA nixpkgs.vimPlugins.lualine-nvim
+nix-env -iA nixpkgs.vimPlugins.cmp-nvim-lsp
+nix-env -iA nixpkgs.vimPlugins.cmp-buffer
+nix-env -iA nixpkgs.vimPlugins.formatter-nvim
+# FIXME I did not find https://github.com/numToStr/Comment.nvim.git in nixpkgs
+
+# Colorschemes
+nix-env -iA nixpkgs.vimPlugins.sonokai
+nix-env -iA nixpkgs.vimPlugins.everforest
+
+# TODO I still need the following plugins?
+# https://github.com/leafgarland/typescript-vim.git
+# https://github.com/rust-lang/rust.vim.git
 
 # TODO check out https://github.com/nvim-telescope/telescope.nvim.git
 # TODO check out https://github.com/akinsho/toggleterm.nvim
