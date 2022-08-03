75b0d1eb48db5c18977bcfcf695d56fa95dcf383
Louis Varin <109242973+louib-bitgo@users.noreply.github.com>
2022-08-02 22:57:08 -0400

    Add a config file for stylua and format init.lua (#12)
    * Add a config file for stylua and format init.lua
    
    * Enable alejandra nix linting
    
    * Add LSP status to status bar.

--- assets/vim/init.lua
+++ assets/vim/init.lua
@@ -1,8 +1,10 @@
+local LSP_ENABLED_VAR_NAME = 'LSP_ENABLED'
+
 local function get_project_name()
   local current_dir = vim.fn.getcwd()
 
   local last_dir_name = nil
-  for dir_name in string.gmatch(current_dir, "//") do
+  for dir_name in string.gmatch(current_dir, '//') do
     last_dir_name = dir_name
     print(dir_name)
   end
@@ -12,7 +14,7 @@ end
 
 local function set_filetype_options()
   local buffer_number = vim.api.nvim_get_current_buf()
-  local filetype = vim.api.nvim_buf_get_option(buffer_number, "filetype")
+  local filetype = vim.api.nvim_buf_get_option(buffer_number, 'filetype')
 
   -- Default if we cannot detect the filetype.
   vim.wo.spell = false
@@ -21,40 +23,40 @@ local function set_filetype_options()
   vim.bo.expandtab = true
 
   -- TODO not sure that this makes sense just yet.
-  vim.wo.colorcolumn = "100"
+  vim.wo.colorcolumn = '100'
 
-  if filetype == "sh" then
-    vim.bo.makeprg = "shellcheck -f gcc %"
+  if filetype == 'sh' then
+    vim.bo.makeprg = 'shellcheck -f gcc %'
     vim.bo.tabstop = 4
     vim.bo.shiftwidth = 4
     return
   end
 
-  if filetype == "cpp" then
+  if filetype == 'cpp' then
     vim.bo.tabstop = 4
     vim.bo.shiftwidth = 4
     return
   end
 
-  if filetype == "python" then
+  if filetype == 'python' then
     vim.bo.tabstop = 4
     vim.bo.shiftwidth = 4
     return
   end
 
-  if filetype == "gitcommit" then
-    vim.wo.colorcolumn = "50"
+  if filetype == 'gitcommit' then
+    vim.wo.colorcolumn = '50'
     return
   end
 
   -- Running spell checking by default on specific text files.
   -- See https://vimtricks.com/p/vimtrick-spell-checking-in-vim/ for
   -- the spell checking shortcuts.
-  if filetype == "tex" then
+  if filetype == 'tex' then
     vim.wo.spell = true
     return
   end
-  if filetype == "markdown" then
+  if filetype == 'markdown' then
     vim.wo.spell = true
     return
   end
@@ -66,22 +68,22 @@ local function escape_termcode(raw_termcode)
 end
 
 local function configure_auto_format()
-  if not pcall(require, "formatter") then
-    print("formatter is not installed.")
+  if not pcall(require, 'formatter') then
+    print('formatter is not installed.')
     return
   end
-  if not pcall(require, "formatter.util") then
-    print("formatter.util is not installed.")
+  if not pcall(require, 'formatter.util') then
+    print('formatter.util is not installed.')
     return
   end
 
   -- Utilities for creating configurations
-  local formatter_util = require "formatter.util"
+  local formatter_util = require('formatter.util')
 
   -- Provides the Format and FormatWrite commands
   -- See https://github.com/mhartington/formatter.nvim#configuration-specification
   -- for all the configuration options.
-  require('formatter').setup {
+  require('formatter').setup({
     -- Enable or disable logging
     logging = true,
     -- Set the log level
@@ -92,56 +94,64 @@ local function configure_auto_format()
       cpp = {
         function()
           return {
-            exe = "clang-format",
+            exe = 'clang-format',
             stdin = true,
           }
-        end
+        end,
       },
       rust = {
         function()
           return {
-            exe = "rustfmt",
+            exe = 'rustfmt',
             stdin = true,
           }
-        end
+        end,
+      },
+      nix = {
+        function()
+          return {
+            exe = 'alejandra',
+            stdin = true,
+            args = {
+              '-q',
+              '-',
+            },
+          }
+        end,
       },
       lua = {
-        -- Pick from defaults:
-        require('formatter.filetypes.lua').stylua,
-
         -- ,or define your own:
         function()
           return {
-            exe = "stylua",
+            exe = 'stylua',
             args = {
-              "--search-parent-directories",
-              "--stdin-filepath",
+              '--search-parent-directories',
+              '--stdin-filepath',
               formatter_util.escape_path(formatter_util.get_current_buffer_file_path()),
-              "--",
-              "-",
+              '--',
+              '-',
             },
             stdin = true,
           }
-        end
-      }
-    }
-  }
+        end,
+      },
+    },
+  })
 
   -- this will format and write the buffer after saving.
-  vim.cmd[[
+  vim.cmd([[
     augroup FormatAutogroup
       autocmd!
       autocmd BufWritePost * FormatWrite
     augroup END
-  ]]
+  ]])
 end
 
-
 local function configure_auto_completion()
-  vim.go.omnifunc = "syntaxcomplete#Complete"
+  vim.go.omnifunc = 'syntaxcomplete#Complete'
 
-  if not pcall(require, "cmp") then
-    print("cmp is not installed.")
+  if not pcall(require, 'cmp') then
+    print('cmp is not installed.')
     return
   end
 
@@ -152,7 +162,7 @@ local function configure_auto_completion()
     snippet = {
       -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        vim.fn['vsnip#anonymous'](args.body) -- For `vsnip` users.
         -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
         -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
         -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
@@ -167,7 +177,8 @@ local function configure_auto_completion()
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ['<CR>'] = cmp.mapping.confirm({ select = false }),
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
@@ -177,7 +188,7 @@ local function configure_auto_completion()
       -- { name = 'snippy' }, -- For snippy users.
     }, {
       { name = 'buffer' },
-    })
+    }),
   })
 
   -- Set configuration for specific filetype.
@@ -186,45 +197,45 @@ local function configure_auto_completion()
       { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
     }, {
       { name = 'buffer' },
-    })
+    }),
   })
 
   -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
   cmp.setup.cmdline('/', {
     mapping = cmp.mapping.preset.cmdline(),
     sources = {
-      { name = 'buffer' }
-    }
+      { name = 'buffer' },
+    },
   })
 
   -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
   cmp.setup.cmdline(':', {
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
-      { name = 'path' }
+      { name = 'path' },
     }, {
-      { name = 'cmdline' }
-    })
+      { name = 'cmdline' },
+    }),
   })
 
   vim.o.completeopt = 'menu,menuone,noselect'
 end
 
 local function configure_status_bar()
-  if not pcall(require, "lualine") then
-    print("lualine is not installed.")
+  if not pcall(require, 'lualine') then
+    print('lualine is not installed.')
     return
   end
 
   -- https://github.com/nvim-lualine/lualine.nvim#available-components
   -- for the available components.
-  require('lualine').setup {
+  require('lualine').setup({
     options = {
       -- FIXME what do I need to enable the icons?
       icons_enabled = false,
       theme = 'dracula',
-      component_separators = { left = '', right = ''},
-      section_separators = { left = '', right = ''},
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
       disabled_filetypes = {},
       always_divide_middle = true,
       globalstatus = false,
@@ -234,35 +245,47 @@ local function configure_status_bar()
         {
           'mode',
           padding = 2,
-        }
+        },
       },
-      lualine_b = {'branch', 'diff', 'diagnostics'},
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
       lualine_c = {
         {
           'filename',
           padding = 2,
         },
       },
-      lualine_x = {'encoding', 'fileformat', 'filetype'},
-      lualine_y = {'progress'},
-      lualine_z = {'location'}
+      lualine_x = {
+        'encoding',
+        'fileformat',
+        'filetype',
+        function()
+          local success, response = pcall(vim.api.nvim_buf_get_var, 0, LSP_ENABLED_VAR_NAME)
+          if not success or not response then
+            return '[LSP disabled]'
+          end
+
+          return '[LSP enabled]'
+        end,
+      },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' },
     },
     inactive_sections = {
       lualine_a = {},
       lualine_b = {},
-      lualine_c = {'filename'},
-      lualine_x = {'location'},
+      lualine_c = { 'filename' },
+      lualine_x = { 'location' },
       lualine_y = {},
-      lualine_z = {}
+      lualine_z = {},
     },
     tabline = {},
-    extensions = {}
-  }
+    extensions = {},
+  })
 end
 
 local function configure_lsp()
-  if not pcall(require, "lspconfig") then
-    print("lspconfig is not installed.")
+  if not pcall(require, 'lspconfig') then
+    print('lspconfig is not installed.')
     return
   end
 
@@ -277,7 +300,7 @@ local function configure_lsp()
 
     -- Mappings.
     -- See `:h :map-arguments` for the options available when mapping
-    local mapping_options = { noremap=true, silent=true, buffer=buffer_number }
+    local mapping_options = { noremap = true, silent = true, buffer = buffer_number }
 
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, mapping_options)
@@ -285,11 +308,12 @@ local function configure_lsp()
 
     vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, mapping_options)
 
+    vim.api.nvim_buf_set_var(buffer_number, LSP_ENABLED_VAR_NAME, true)
   end
 
   local lsp_flags = {
-      -- This is the default in Nvim 0.7+
-      debounce_text_changes = 150,
+    -- This is the default in Nvim 0.7+
+    debounce_text_changes = 150,
   }
 
   local rust_analyzer_features = {}
@@ -299,26 +323,26 @@ local function configure_lsp()
   --   rust_analyzer_features = {"allo"}
   -- end
 
-  require('lspconfig').rust_analyzer.setup{
+  require('lspconfig').rust_analyzer.setup({
     on_attach = custom_lsp_attach,
     flags = lsp_flags,
     -- Server-specific settings...
     settings = {
-      ["rust-analyzer"] = {
+      ['rust-analyzer'] = {
         cargo = {
           features = rust_analyzer_features,
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  })
 
-  require('lspconfig').clangd.setup{
+  require('lspconfig').clangd.setup({
     on_attach = custom_lsp_attach,
     flags = lsp_flags,
     settings = {
-      ["clangd"] = {}
-    }
-  }
+      ['clangd'] = {},
+    },
+  })
 
   -- See https://github.com/neovim/nvim-lspconfig#suggested-configuration for
   -- the suggested top-level configuration and https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
@@ -341,42 +365,42 @@ local function configure_key_bindings()
   -- See all the available options here https://neovim.io/doc/user/map.html#:map-arguments
 
   -- Window navigation and creation
-  vim.api.nvim_set_keymap("n", "<Space>s", "<C-w>s", { silent = true, noremap = true })
-  vim.api.nvim_set_keymap("n", "<Space>v", "<C-w>v", { silent = true, noremap = true })
-  vim.api.nvim_set_keymap("n", "<Space>c", "<C-w>c", { silent = true, noremap = true })
-  vim.api.nvim_set_keymap("n", "<Space>j", "<C-w>j", { silent = true, noremap = true })
-  vim.api.nvim_set_keymap("n", "<Space>k", "<C-w>k", { silent = true, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>s', '<C-w>s', { silent = true, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>v', '<C-w>v', { silent = true, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>c', '<C-w>c', { silent = true, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>j', '<C-w>j', { silent = true, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>k', '<C-w>k', { silent = true, noremap = true })
 
   -- Buffer navigation and management
-  vim.api.nvim_set_keymap("n", "<Space>w", ":bdelete<Enter>", { silent = false, noremap = true })
-  vim.api.nvim_set_keymap("n", "<Space>h", ":bprevious<Enter>", { silent = false, noremap = true })
-  vim.api.nvim_set_keymap("n", "<Space>l", ":bnext<Enter>", { silent = false, noremap = true })
-  vim.api.nvim_set_keymap("n", "<Space>1", ":bfirst<Enter>", { silent = false, noremap = true })
-  vim.api.nvim_set_keymap("n", "<Space>2", ":e #2<Enter>", { silent = false, noremap = true })
-  vim.api.nvim_set_keymap("n", "<Space>3", ":e #3<Enter>", { silent = false, noremap = true })
-  vim.api.nvim_set_keymap("n", "<Space>4", ":e #4<Enter>", { silent = false, noremap = true })
-  vim.api.nvim_set_keymap("n", "<Space>5", ":e #5<Enter>", { silent = false, noremap = true })
-  vim.api.nvim_set_keymap("n", "<Space>6", ":e #6<Enter>", { silent = false, noremap = true })
-  vim.api.nvim_set_keymap("n", "<Space>7", ":e #7<Enter>", { silent = false, noremap = true })
-  vim.api.nvim_set_keymap("n", "<Space>8", ":e #8<Enter>", { silent = false, noremap = true })
-  vim.api.nvim_set_keymap("n", "<Space>9", ":e #9<Enter>", { silent = false, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>w', ':bdelete<Enter>', { silent = false, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>h', ':bprevious<Enter>', { silent = false, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>l', ':bnext<Enter>', { silent = false, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>1', ':bfirst<Enter>', { silent = false, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>2', ':e #2<Enter>', { silent = false, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>3', ':e #3<Enter>', { silent = false, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>4', ':e #4<Enter>', { silent = false, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>5', ':e #5<Enter>', { silent = false, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>6', ':e #6<Enter>', { silent = false, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>7', ':e #7<Enter>', { silent = false, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>8', ':e #8<Enter>', { silent = false, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>9', ':e #9<Enter>', { silent = false, noremap = true })
 
   -- This unsets the "last search pattern" register by hitting return
   -- Credits to https://stackoverflow.com/a/662914
-  vim.api.nvim_set_keymap("n", "<CR>", ":noh<CR><CR>", { silent = false, noremap = true })
+  vim.api.nvim_set_keymap('n', '<CR>', ':noh<CR><CR>', { silent = false, noremap = true })
 
-  vim.api.nvim_set_keymap("", "<C-c>", "\"+y", { silent = false, noremap = false })
+  vim.api.nvim_set_keymap('', '<C-c>', '"+y', { silent = false, noremap = false })
 
   -- Using the 'q' buffer as the quick buffer, with easy re-apply!
   -- Start recording that buffer with `qq`.
   -- Stop recording with `q`.
   -- Apply with space+q !!!
-  vim.api.nvim_set_keymap("n", "<Space>q", "@q", { silent = false, noremap = true })
+  vim.api.nvim_set_keymap('n', '<Space>q', '@q', { silent = false, noremap = true })
 end
 
 local function configure_commenting()
-  if not pcall(require, "Comment") then
-    print("Comment is not installed.")
+  if not pcall(require, 'Comment') then
+    print('Comment is not installed.')
     return
   end
 
@@ -438,24 +462,24 @@ local function configure_global_options()
   -- FIXME switch to rg when I installed it in the neovim container.
   -- vim.go.grepprg = "rg --vimgrep"
   -- vim.go.grepformat = "%f:%l:%c:%m"
-  vim.go.grepprg = "grep --binary-files=without-match --exclude-dir=target/ --exclude-dir=.git/ -rni"
+  vim.go.grepprg = 'grep --binary-files=without-match --exclude-dir=target/ --exclude-dir=.git/ -rni'
 
   -- The shusia, maia and espresso variants exist for the sonokai colorscheme
   -- FIXME how to change the colorscheme variant?
-  local colorscheme = "sonokai"
+  local colorscheme = 'sonokai'
   -- local colorscheme = "everforest"
 
-  pcall(vim.cmd, "colorscheme " .. colorscheme)
+  pcall(vim.cmd, 'colorscheme ' .. colorscheme)
 end
 
 local function configure_nvim()
   vim.api.nvim_command('syntax on')
 
   -- Enable filetype detection
-  vim.cmd [[
+  vim.cmd([[
     filetype plugin on
     filetype plugin indent on
-  ]]
+  ]])
 
   -- Calling packloadall is not necessary, because it will be called after
   -- running the init.lua anyway. Leaving here in case we want to load the plugins
@@ -463,20 +487,20 @@ local function configure_nvim()
   -- vim.api.nvim_command('packloadall')
   --
 
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = "*",
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = '*',
     callback = function()
       vim.schedule(set_filetype_options)
     end,
   })
 
-  vim.cmd [[
+  vim.cmd([[
     augroup quickfix
         autocmd!
         autocmd QuickFixCmdPost [^l]* cwindow
         autocmd QuickFixCmdPost l* lwindow
     augroup END
-  ]]
+  ]])
 end
 
 local function configure()
@@ -486,8 +510,8 @@ local function configure()
   configure_auto_format()
   configure_auto_completion()
   configure_commenting()
-  configure_status_bar()
   configure_lsp()
+  configure_status_bar()
 end
 
 configure()
