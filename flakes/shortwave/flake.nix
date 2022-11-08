e6357bd623d48582146249105ac296bebdd986e2
louib <code@louib.net>
2022-11-08 09:19:11 -0500

    Add Shortwave development flake

---
+++ flakes/shortwave/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Shortwave version with the reconnection fix";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs";
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  };
+}
