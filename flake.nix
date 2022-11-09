86b3448e8cb8ccc53e75825991ba558fb40e7a8c
louib <code@louib.net>
2022-11-09 09:44:14 -0500

    Add a top level flake for system configuration

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "Configuration for my main systems";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  };
+}
