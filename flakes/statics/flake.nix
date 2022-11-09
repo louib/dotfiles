22cce67a6ae15b1a8c22d5de319dad7d0b49491d
louib <code@louib.net>
2022-11-09 11:09:34 -0500

    Remove nixpkgs from statics inputs

--- flakes/statics/flake.nix
+++ flakes/statics/flake.nix
@@ -1,13 +1,10 @@
 {
   description = "Static values in my Nix configuration";
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+  outputs = {self}: {
+    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
     lib = {
       # This list is derived from https://github.com/numtide/flake-utils/blob/5aed5285a952e0b949eb3ba02c12fa4fcfef535f/default.nix#L3
