ba1da56d7a5daedfc610d60e925445c9f570b3dd
louib <code@louib.net>
2022-11-12 10:28:01 -0500

    fix: use my own default systems

--- flakes/keepassxc/flake.nix
+++ flakes/keepassxc/flake.nix
@@ -2,8 +2,14 @@
   description = "Flake for KeePassXC development";
 
   inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs";
+    };
     flake-utils = {
       url = "github:numtide/flake-utils";
+    };
+    statics = {
+      url = "github:louib/dotfiles?dir=flakes/statics";
     };
   };
 
@@ -11,36 +17,39 @@
     self,
     nixpkgs,
     flake-utils,
+    statics,
   }: (
-    flake-utils.lib.eachDefaultSystem (
+    flake-utils.lib.eachSystem statics.lib.defaultSystems (
       system: (
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in {
-          devShell = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              gnumake
-              cmake
-              gcc8
-              curl
-              botan2
-              xorg.libXtst
-              xorg.libXi
-              libargon2
-              libusb1
-              minizip
-              pcsclite
-              qrencode
-              asciidoctor
-              libsForQt5.qt5.qtbase
-              libsForQt5.qt5.qttools
-              libsForQt5.qt5.qttranslations
-              libsForQt5.qt5.qtsvg
-              libsForQt5.qt5.qtx11extras
-              libsForQt5.qt5.qtwayland
-              readline
-              zlib
-            ];
+          devShells = {
+            default = pkgs.mkShell {
+              buildInputs = with pkgs; [
+                gnumake
+                cmake
+                gcc8
+                curl
+                botan2
+                xorg.libXtst
+                xorg.libXi
+                libargon2
+                libusb1
+                minizip
+                pcsclite
+                qrencode
+                asciidoctor
+                libsForQt5.qt5.qtbase
+                libsForQt5.qt5.qttools
+                libsForQt5.qt5.qttranslations
+                libsForQt5.qt5.qtsvg
+                libsForQt5.qt5.qtx11extras
+                libsForQt5.qt5.qtwayland
+                readline
+                zlib
+              ];
+            };
           };
         }
       )
