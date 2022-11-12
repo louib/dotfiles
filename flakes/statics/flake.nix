2718c051ea51851d93108d2ca94023c1caf93848
louib <code@louib.net>
2022-11-12 11:24:28 -0500

    chore: cleanup statics flake

--- flakes/statics/flake.nix
+++ flakes/statics/flake.nix
@@ -2,10 +2,6 @@
   description = "Static values in my Nix configuration";
 
   outputs = {self}: {
-    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
     lib = {
       # This list is derived from https://github.com/numtide/flake-utils/blob/5aed5285a952e0b949eb3ba02c12fa4fcfef535f/default.nix#L3
       # from which I removed the Darwin systems.
@@ -16,6 +12,7 @@
 
       defaultUsername = "louib";
       # TODO should I have my GPG public key here?
+      # TODO should I have my SSH public key here?
     };
   };
 }
