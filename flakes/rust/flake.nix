55cc5f01cf0f4f1dcddf70e75fefdc9c86fe1a9f
louib <code@louib.net>
2022-11-12 11:03:22 -0500

    feat: add a flake for rust dev

---
+++ flakes/rust/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Flake for Rust development";
+
+  inputs = {
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: (
+    flake-utils.lib.eachDefaultSystem (
+      system: (
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cargo
+              rustc
+              rustfmt
+            ];
+          };
+        }
+      )
+    )
+  );
+}
