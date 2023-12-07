a0b4532218d0693a5c2d165f219f5e79e5640d1d
louib <code@louib.net>
2023-12-06 23:37:15 -0500

    feat: more wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,7 +13,42 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
+-- config.color_scheme = 'AdventureTime'
+-- config.color_scheme = 'Bespin (base16)'
+-- config.color_scheme = 'Black Metal (base16)'
+-- config.color_scheme = 'Brush Trees Dark (base16)'
+
+config.keys = {
+  {
+    key = 't',
+    mods = 'CTRL',
+    action = wezterm.action.SpawnTab('DefaultDomain'),
+  },
+  { key = 'h', mods = 'CTRL', action = wezterm.action.ActivateTabRelative(-1) },
+  { key = 'l', mods = 'CTRL', action = wezterm.action.ActivateTabRelative(1) },
+}
+
+config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+
+config.tab_bar_at_bottom = false
+
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 0,
+  bottom = 0,
+}
+
+for i = 1, 8 do
+  -- CTRL+ALT + number to activate that tab
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'ALT',
+    action = wezterm.action.ActivateTab(i - 1),
+  })
+end
+
+config.colors = {}
 
 -- and finally, return
 --
