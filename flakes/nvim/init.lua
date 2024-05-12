b2572460598d5342fec9cd8a78688a72bc6018fe
louib <code@louib.net>
2024-05-12 14:32:54 -0400

    feat(nvim): add wq as exit sequence

--- flakes/nvim/init.lua
+++ flakes/nvim/init.lua
@@ -930,7 +930,9 @@ local function configure_key_bindings()
   vim.api.nvim_set_keymap('n', '<C-b>', ':GitBlameCopyFileURL<Enter>', { silent = false, noremap = true })
 
   if os.getenv('NVIM_EMBEDDED') == 'true' then
-    vim.api.nvim_set_keymap('c', 'w<Enter>', 'w >> /dev/stderr<Enter>:q!<Enter>', { silent = false, noremap = true })
+    local embedded_exit_sequence = 'w >> /dev/stderr<Enter>:q!<Enter>'
+    vim.api.nvim_set_keymap('c', 'w<Enter>', embedded_exit_sequence, { silent = false, noremap = true })
+    vim.api.nvim_set_keymap('c', 'wq<Enter>', embedded_exit_sequence, { silent = false, noremap = true })
   end
 end
 
