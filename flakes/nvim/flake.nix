59e2ab9b1260e61b14109ace012407dbbdfe83b7
Louis Varin <louisvarin@bitgo.com>
2024-11-28 10:50:04 +0530

    feat(nvim): bump inputs and node

--- flakes/nvim/flake.nix
+++ flakes/nvim/flake.nix
@@ -24,7 +24,7 @@
             # xclip (or wl-clipboard) is required to clip to the system clipboard.
             inherit xclip;
             # Copilot requires a version of Node.js > 16.x
-            inherit nodejs-18_x;
+            inherit nodejs_20;
             inherit nil;
             inherit rust-analyzer;
             inherit cargo;
