13fb8c95b62023e14573949bcb9a15a68a816809
louib <code@louib.net>
2025-10-11 19:03:16 -0400

    chore: improve starship style

--- flakes/shell-config/starship.nix
+++ flakes/shell-config/starship.nix
@@ -2,7 +2,7 @@
   # Get editor completions based on the config schema
   "$schema" = "https://starship.rs/config-schema.json";
 
-  format = "$username$hostname$time$directory$git_branch$git_state$git_status$nix_shell$character";
+  format = ''$username$hostname$time$directory$git_branch$git_state$git_status$nix_shell $character'';
 
   # Disable the blank line at the start of the prompt
   add_newline = false;
@@ -11,42 +11,43 @@
     show_always = true;
     style_user = "bold fg:#${colors.starship.default-fg} bg:#${colors.starship.user-bg}";
     style_root = "bold fg:#${colors.starship.default-fg} bg:#${colors.starship.root-user-bg}";
-    format = "[ $user ]($style)";
+    format = "[ $user ]($style)[](fg:#${colors.starship.user-bg})";
   };
 
   hostname = {
     ssh_only = true;
-    format = "[ $hostname ]($style)";
     style = "bold fg:#${colors.starship.default-fg} bg:#${colors.starship.hostname-bg}";
+    format = "[](fg:#282828 bg:#${colors.starship.hostname-bg})[ $hostname ]($style)[](fg:#${colors.starship.hostname-bg})";
   };
 
   time = {
     disabled = false;
     time_format = "%X";
     style = "bold fg:#${colors.starship.default-fg} bg:#${colors.starship.time-bg}";
-    format = "[ $time ]($style)";
+    format = "[](fg:#282828 bg:#${colors.starship.time-bg})[ $time ]($style)[](fg:#${colors.starship.time-bg})";
   };
 
   directory = {
-    format = "[ $path ]($style)";
     style = "bold fg:#${colors.starship.default-fg} bg:#${colors.starship.directory-bg}";
+    format = "[](fg:#282828 bg:#${colors.starship.directory-bg})[ $path ]($style)[](fg:#${colors.starship.directory-bg})";
     truncation_length = 3;
     truncation_symbol = "…/";
   };
 
   git_branch = {
-    format = "[( $branch)]($style)";
-    style = "bold fg:#${colors.starship.default-fg} bg:#${colors.starship.git-branch-bg}";
     symbol = "";
+    style = "bold fg:#${colors.starship.default-fg} bg:#${colors.starship.git-branch-bg}";
+    format = "[](fg:#282828 bg:#${colors.starship.git-branch-bg})[ $branch ]($style)";
   };
 
   git_state = {
-    format = "[( $state)]($style)";
     style = "bold fg:#${colors.starship.default-fg} bg:#${colors.starship.git-state-bg}";
+    format = "[$state]($style)";
   };
 
   git_status = {
-    format = "[( $all_status$ahead_behind) ]($style)";
+    style = "bold fg:#${colors.starship.default-fg} bg:#${colors.starship.git-status-bg}";
+    format = "[ $all_status$ahead_behind ]($style)[](fg:#${colors.starship.git-status-bg})";
     ahead = ">";
     behind = "<";
     deleted = "-";
@@ -56,19 +57,18 @@
     staged = "+";
     conflicted = "!";
     stashed = "";
-    style = "bold fg:#${colors.starship.default-fg} bg:#${colors.starship.git-status-bg}";
     up_to_date = "✓";
   };
 
   nix_shell = {
-    style = "bold fg:#${colors.starship.nix-prompt-fg} bg:#${colors.starship.nix-prompt-bg}";
     symbol = "nix";
-    format = "[ $symbol ]($style)";
+    style = "bold fg:#${colors.starship.nix-prompt-fg} bg:#${colors.starship.nix-prompt-bg}";
+    format = "[](fg:#282828 bg:#${colors.starship.nix-prompt-bg})[ $symbol ]($style)[](fg:#${colors.starship.nix-prompt-bg})";
   };
 
   character = {
-    success_symbol = "[ ](bold fg:#${colors.starship.success-symbol-fg} bg:#${colors.starship.success-symbol-bg})";
-    error_symbol = "[ ](bold fg:#${colors.starship.default-fg} bg:#${colors.starship.error-symbol-bg})";
+    success_symbol = "[❯](bold fg:#${colors.starship.success-symbol-bg})";
+    error_symbol = "[❯](bold fg:#${colors.starship.error-symbol-bg})";
     # FIXME vicmd_symbol is not supported on bash :(
     # https://starship.rs/config/#character
     # vicmd_symbol = "[ ](bg:green)";
