7b61cd284dbf419512fc7b5c202cdc2af9c05715
Louis Varin <code@louib.net>
2026-05-07 15:36:01 -0400

    feat(wezterm): enable hide_tab_bar_if_only_one_tab

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,6 +10,8 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+config.hide_tab_bar_if_only_one_tab = true
+
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
